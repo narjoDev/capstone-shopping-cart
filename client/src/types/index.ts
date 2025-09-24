67eade2dd104504f64ffcae3a5b9d91968acadf6
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-09-24 10:47:16 -0700

    install zod and convert types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,12 +1,23 @@
-export interface Product {
-  _id: string;
-  title: string;
-  quantity: number;
-  price: number;
-}
+import { z } from "zod";
 
-export interface CartItem
-  extends Pick<Product, "title" | "quantity" | "price"> {
-  _id: string;
-  productId: Product["_id"];
-}
+export const productSchema = z.object({
+  _id: z.string(),
+  title: z.string(),
+  quantity: z.number(),
+  price: z.number(),
+});
+
+export type Product = z.infer<typeof productSchema>;
+
+export const cartItemSchema = productSchema
+  .pick({
+    title: true,
+    quantity: true,
+    price: true,
+  })
+  .extend({
+    _id: z.string(),
+    productId: productSchema.shape._id,
+  });
+
+export type CartItem = z.infer<typeof cartItemSchema>;
