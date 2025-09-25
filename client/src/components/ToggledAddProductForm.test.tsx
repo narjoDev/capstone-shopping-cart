d6516bf6bedf7c268bba49c2710d9dc44aa4be2c
narjoDev <63086420+narjoDev@users.noreply.github.com>
2025-09-25 16:42:11 -0700

    add tests for ToggledAddProductForm

--- client/src/components/ToggledAddProductForm.test.tsx
+++ client/src/components/ToggledAddProductForm.test.tsx
@@ -1,4 +1,41 @@
-// import ToggledAddProductForm from "./ToggledAddProductForm";
+import ToggledAddProductForm from "./ToggledAddProductForm";
 
-// import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+it("displays add product button", () => {
+  const props = { addProduct: () => {} };
+  render(<ToggledAddProductForm {...props} />);
+  const addButton = screen.getByRole("button", { name: /Add A Product/i });
+  expect(addButton).toBeInTheDocument();
+});
+
+it("hides button and displays form when add button clicked", async () => {
+  const props = { addProduct: () => {} };
+  render(<ToggledAddProductForm {...props} />);
+  const user = userEvent.setup();
+
+  const addButton = screen.getByRole("button", { name: /Add A Product/i });
+  await user.click(addButton);
+  expect(addButton).not.toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", { name: /Product Name/ });
+  expect(nameInput).toBeInTheDocument();
+});
+
+it("hides form when cancel clicked", async () => {
+  const props = { addProduct: () => {} };
+  render(<ToggledAddProductForm {...props} />);
+  const user = userEvent.setup();
+
+  const addButton = screen.getByRole("button", { name: /Add A Product/i });
+  await user.click(addButton);
+
+  const cancelButton = screen.getByRole("button", { name: /Cancel/ });
+  expect(cancelButton).toBeInTheDocument();
+  await user.click(cancelButton);
+
+  expect(cancelButton).not.toBeInTheDocument();
+  const newAddButton = screen.getByRole("button", { name: /Add A Product/i });
+  expect(newAddButton).toBeInTheDocument();
+});
